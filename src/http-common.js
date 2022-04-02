10f277fcaf47fa3def435754e2d8adcc8e1f484d
marcosgmsantos <102864873+marcosgmsantos@users.noreply.github.com>
2022-04-02 21:49:18 +0500

    integration of backend apis

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://caabackend.herokuapp.com/",
+  baseURL: "https://caabackend.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
