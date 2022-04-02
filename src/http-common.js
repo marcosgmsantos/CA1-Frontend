028f5c3d018ac2d3b57d6cfec49bb2d78323531a
marcosgmsantos <102864873+marcosgmsantos@users.noreply.github.com>
2022-04-02 21:43:50 +0500

    updated backend url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3030/api",
+  baseURL: "https://caabackend.herokuapp.com/",
   headers: {
     "Content-type": "application/json"
   }
